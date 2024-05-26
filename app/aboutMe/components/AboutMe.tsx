954e87f61364ee5e421e17d67ff0f871db7aa3f6
Samantha <samantha@uppointinc.com>
2024-05-25 18:13:49 -0700

    fixed naming for about me

--- app/aboutMe/components/AboutMe.tsx
+++ app/aboutMe/components/AboutMe.tsx
@@ -2,7 +2,7 @@ import RootLayout from "@/app/layout";
 import React from "react";
 import Intro from "./Intro";
 
-export default function AboutUs(){
+export default function AboutMe(){
     return(
        <>
             <header className="min-h-screen min-w-full bg-neutral-400" >
