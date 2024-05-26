c556ca5205c61ac7680fa10287c5e2fb43213fd9
Samantha <samantha@uppointinc.com>
2024-05-25 18:15:53 -0700

    fixed aboutme bc removed things to make new branch

---
+++ app/aboutMe/components/AboutMe.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function AboutMe(){
+    return(
+       <>
+            <header className="min-h-screen min-w-full bg-neutral-400" >
+                <div id="about">
+                    <div className="flex ">
+                    
+                    </div>
+                
+                </div>
+            </header>
+   
+       </>
+    )
+}
