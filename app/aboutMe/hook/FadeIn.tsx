a4453400dfd93e56e4b0b8408487a114e59730a5
Samantha <samantha@uppointinc.com>
2024-06-02 10:02:21 -0700

    committing aboutme/hook/ to use built in next font

---
+++ app/aboutMe/hook/FadeIn.tsx
@@ -0,0 +1,46 @@
+'use client'
+import React, { ReactNode, useEffect, useRef, useState } from "react";
+
+interface FadeInProps {
+    children: ReactNode;
+    delayClass : string;
+    // durationClass : string;
+
+}
+
+export default function FadeIn({children, delayClass}: FadeInProps){
+    
+    const [isVisible, setIsVisible] = useState(true);
+    const ref = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        // console.log('useEffect running')
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => 
+                setIsVisible(entry.isIntersecting));
+                // console.log('entries',entries)
+                // if (entry.isIntersecting){
+                //     console.log('intersecting')
+                //         setIsVisible(true);
+                //         observer.disconnect();
+                // }
+                  });
+        if (ref.current) {
+            observer.observe(ref.current);
+        }
+        return() => {
+            if (ref.current){
+                observer.unobserve(ref.current);
+
+            }
+        };
+            }, []);
+        
+     
+    return(
+        <div ref={ref}
+        className={`transform-opacity duration-500  ${delayClass}  ease-in-out ${isVisible ? "opacity-100 " : "opacity-0"}`}>
+            {children}
+        </div>
+    )
+}
