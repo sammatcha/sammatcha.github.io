f7928bb9b352783c10feb7c5deb219c9616295b3
Samantha <samantha@uppointinc.com>
2026-01-01 15:41:45 -0800

    changing timeline and adding more projects

--- app/work/Work.tsx
+++ app/work/Work.tsx
@@ -11,12 +11,13 @@ export default function Work(){
     return(
         <div className="min-w-full min-h-screen px-10 py-10" id="work">
             
-            <div className="justify-center flex flex-col items-center min-w-full">
-               <div className="flex flex-col items-center lg:mt-5 mb-10">
+            <div className="justify-center flex flex-col items-center w-full">
+               <div className="flex flex-col justify-center items-center lg:mt-5 mb-10">
                     <h1 className="text-2xl lg:text-4xl ">Timeline </h1>
                     <div className="border-b w-10 lg:w-20 lg:ms-5"></div>
+                    <Timeline/>
                 </div>
-                <Timeline/>
+                
             </div>
            
                                 {/* CONNECT */}
