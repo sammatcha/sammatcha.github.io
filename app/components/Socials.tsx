ee7bb2db9b5afeeb4753a1592a0ce14d5fb78b5e
Samantha <samantha@uppointinc.com>
2026-02-27 12:54:03 -0800

    fixed links and added docs

--- app/components/Socials.tsx
+++ app/components/Socials.tsx
@@ -4,14 +4,15 @@ import { useRouter } from "next/navigation";
 import Link from "next/link";
 
 const links = [
-    {icon: "[in]",  href:"https://www.linkedin.com/in/samanthakang079/"},
+    {icon: "[in]",  href:"https://www.linkedin.com/in/samanthakang079"},
     {icon: "[gh]", href:"https://github.com/sammatcha"},
+    {icon: "[docs]", href:"https://sammatcha.github.io/dev-notes"}
 ]
 
 export default function Socials(){
     return (
    <div className="relative mt-2 flex items-center justify-center ">
-            <div className="space-y-2">
+            <div className="space-y-1 ">
                 {links.map((link)=> (
                     <Link 
                     key = {link.icon}
